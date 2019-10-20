2f04a1319fa9ebe4af7dff388f5209045c5534a0
Rachel T <rachmaybe@gmail.com>
2019-10-20 20:22:21 +0000

    Updated Dependencies

--- lib/Alerts.rb
+++ lib/Alerts.rb
@@ -3,6 +3,10 @@ require 'nokogiri'
 require 'pry'
 require 'open-uri'
 
+require “alerts/version” 
+require “alerts/scraper”
+require “alerts/cli”
+
 module Alerts
   class Error < StandardError; end
   # Your code goes here...
